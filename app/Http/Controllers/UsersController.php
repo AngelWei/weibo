5d69b503c461181f57fd6088efa189bd7512cf28
AngelWei <liuliwei531@gmail.com>
2019-02-16 06:14:18 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,14 @@ class UsersController extends Controller
     {
     	return view('users.show', compact('user'));
     }
+
+    public function store(Request $request)
+    {
+    	this->validate($request, [
+    		'name' => 'required|max:50',
+    		'email' => 'reuqired|email|unique:users|max:255',
+    		'passwrod' => 'required|confirmed|min:6'
+    	]);
+    	return;
+    }
 }
